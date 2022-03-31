6c97a1e6ce93bb79ea72b2a6fc8492554b8f1812
ryleechisholm <89104299+ryleechisholm@users.noreply.github.com>
2022-03-31 09:46:11 -0500

    Changed styling, removed button and image

--- app/static/script.js
+++ app/static/script.js
@@ -23,9 +23,7 @@ function populateContent(items) {
                 <div style="margin-left: 2em; padding-top:3em">
                     <h2 class="itemName">${item.name}</h2>
                     <h2 class="itemPrice">${item.cost}</h2>
-                    <img class="images" src="{% static '${item.image}' %}">
                     <p class="itemDesc">${item.description}</p>
-                    <button style="width: 5em; height: 5em"class="addListBtn">Add To Ticket!!</button>
                 </div>
               </article>
               `
