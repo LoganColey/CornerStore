1f9b5e2cbff6c484d4a71dbcfb7656a3297acd93
ryleechisholm <89104299+ryleechisholm@users.noreply.github.com>
2022-04-05 14:06:31 -0500

    Changed functions
    function fills the page on load, rearranged the function for each type of item

--- app/static/script.js
+++ app/static/script.js
@@ -3,32 +3,91 @@ const options = document.querySelectorAll(".Btn");
 
 options.forEach((m) => {
   m.addEventListener("click", (e) => {
-    console.log(e.target.innerHTML);
-    fetch("/static/json/" + e.target.name + ".json")
-      .then((req) => req.json())
-      .then((data) => {
-        populateContent(data);
-      });
+    if (e.target.innerHTML === "Small") {
+      let leList = [];
+      fetch("/static/food.json")
+        .then((req) => req.json())
+        .then((data) => {
+          for (const item of data) {
+            if (item.type == "small") {
+              leList.push(item);
+            }
+          }
+          populateContent(leList);
+        });
+    } else if (e.target.innerHTML === "Sides") {
+      let leList = [];
+      fetch("/static/food.json")
+        .then((req) => req.json())
+        .then((data) => {
+          for (const item of data) {
+            if (item.type == "side") {
+              leList.push(item);
+            }
+          }
+          populateContent(leList);
+        });
+    } else if (e.target.innerHTML === "Big") {
+      let leList = [];
+      fetch("/static/food.json")
+        .then((req) => req.json())
+        .then((data) => {
+          for (const item of data) {
+            if (item.type == "big") {
+              leList.push(item);
+            }
+          }
+          populateContent(leList);
+        });
+    } else if (e.target.innerHTML === "Salads") {
+      let leList = [];
+      fetch("/static/food.json")
+        .then((req) => req.json())
+        .then((data) => {
+          for (const item of data) {
+            if (item.type == "salad") {
+              leList.push(item);
+            }
+          }
+          populateContent(leList);
+        });
+    } else if (e.target.innerHTML === "Starters") {
+      let leList = [];
+      fetch("/static/food.json")
+        .then((req) => req.json())
+        .then((data) => {
+          for (const item of data) {
+            if (item.type == "starter") {
+              leList.push(item);
+            }
+          }
+          populateContent(leList);
+        });
+    }
   });
 });
 
 function populateContent(items) {
-  console.log(foodbox.innerHTML);
   foodbox.innerHTML = "";
   for (const item of items) {
     foodbox.insertAdjacentHTML(
       "beforeend",
       `
-              <article class = "foodItem" style="background-color: lightgray">
-                <div style="margin-left: 2em; padding-top:3em">
+                <article class="itemBox">
                     <h2 class="itemName">${item.name}</h2>
                     <img src=/static/images/${item.image}></img>
                     <p class="itemDesc">${item.description}</p>
-                    <h2 class="itemPrice">${item.cost}</h2>
-                </div>
-              </article>
+                    <h2 class="itemPrice">$ ${item.cost}</h2>
+                </article>
               `
     );
   }
 }
 
+function populateInitialContent() {
+  fetch("/static/food.json")
+    .then((req) => req.json())
+    .then((data) => {
+      populateContent(data);
+    });
+}
