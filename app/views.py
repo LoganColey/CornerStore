3b5b05b4a8d4a60900f8c6e916b8cc44acfb3b97
LoganColey <lcoley22@basecampcodingacademy.org>
2022-04-06 10:23:20 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -12,6 +12,8 @@ from .forms import *
 from django.contrib.auth.models import Group
 from json import dumps
 from django.core import serializers
+from django.http import JsonResponse
+import json 
 
 @unauthenticated_user
 def login_page(request):
@@ -105,8 +107,18 @@ def food(request):
     return render(request,'food.html',context)
 
 def checkout(request):
-    context ={}
+    cart = Cart.objects.get(user=request.user)
+    foods = bigFood.objects.filter(cart=cart)
+    price = 0
+    for food in foods:
+        food.price += price
+    context = {"price":price}
     return render(request,'checkout.html',context)
+
+def paymentComplete(request):
+    body = json.loads(request.body)
+    print('BODY:', body)
+    return JsonResponse('Payment completed!', safe=False)
 
 def populateBigFood(request):
     return render(request, "food.html")
