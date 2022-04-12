3eb77bf1633bc4865aeeb0e6a1dd727142b5e2df
ryleechisholm <rchisholm22@basecampcodingacademy.org>
2022-04-12 09:08:37 -0500

    labeled styling, attempt to toggle divs

--- CornerStore/urls.py
+++ CornerStore/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('logout/', views.logoutUser, name="logout"),
     path('home/', views.home, name='home'),
     path('food/', views.populateBigFood, name="food"),
-    path('cart/', views.checkout, name="checkout"),
+    path('checkout/', views.checkout, name="checkout"),
     path('complete/', views.paymentComplete, name='complete'),
     path('adminpage/', views.admin, name='admin')
 ]
