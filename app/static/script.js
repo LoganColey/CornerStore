c6d7035b82bc43bbb2b0981a0858f22d11e5473a
ryleechisholm <rchisholm22@basecampcodingacademy.org>
2022-04-18 09:09:59 -0500

    Same functionality, shorter code
    broke some code into functions to shorten the length and make it easier to read

--- app/static/script.js
+++ app/static/script.js
@@ -10,182 +10,74 @@ const sidesNames = [];
 options.forEach((m) => {
   m.addEventListener("click", (e) => {
     if (e.target.innerHTML === "Small") {
-      let leList = [];
-      fetch("/static/food.json")
-        .then((req) => req.json())
-        .then((data) => {
-          for (const item of data) {
-            if (item.type == "small") {
-              leList.push(item);
-            }
-          }
-          foodbox.innerHTML = "";
-          populateContent(leList);
-        });
+      foodbox.innerHTML = "";
+      fetchingAndPopulating("small", "Burgers and More");
     } else if (e.target.innerHTML === "Sides") {
-      let leList = [];
-      fetch("/static/food.json")
-        .then((req) => req.json())
-        .then((data) => {
-          for (const item of data) {
-            if (item.type == "side") {
-              leList.push(item);
-            }
-          }
-          foodbox.innerHTML = "";
-          populateContent(leList);
-        });
+      foodbox.innerHTML = "";
+      fetchingAndPopulating("side", "Sides");
     } else if (e.target.innerHTML === "Big") {
-      let leList = [];
-      fetch("/static/food.json")
-        .then((req) => req.json())
-        .then((data) => {
-          for (const item of data) {
-            if (item.type == "big") {
-              leList.push(item);
-            }
-          }
-          foodbox.innerHTML = "";
-          populateContent(leList);
-        });
+      foodbox.innerHTML = "";
+      fetchingAndPopulating("big", "Plates");
     } else if (e.target.innerHTML === "Salads") {
-      let leList = [];
-      fetch("/static/food.json")
-        .then((req) => req.json())
-        .then((data) => {
-          for (const item of data) {
-            if (item.type == "salad") {
-              leList.push(item);
-            }
-          }
-          foodbox.innerHTML = "";
-          populateContent(leList);
-        });
+      foodbox.innerHTML = "";
+      fetchingAndPopulating("salad", "Salads");
     } else if (e.target.innerHTML === "Starters") {
-      let leList = [];
-      fetch("/static/food.json")
-        .then((req) => req.json())
-        .then((data) => {
-          for (const item of data) {
-            if (item.type == "starter") {
-              leList.push(item);
-            }
-          }
-          foodbox.innerHTML = "";
-          populateContent(leList);
-        });
+      foodbox.innerHTML = "";
+      fetchingAndPopulating("starter", "Starters");
     }
   });
 });
 
-//creates html for items of certain type
-function populateContent(items) {
-  var itemsbox = document.createElement("div");
-  itemsbox.classList.add("items");
-  foodbox.appendChild(itemsbox);
-  for (const item of items) {
-    var roundedCost = item.cost.toFixed(2);
-    itemsbox.insertAdjacentHTML(
-      "beforeend",
-      `
-                <article id="itemBox" class="${item.type}">
-                    <h2 class="itemName">${item.name}</h2>
-                    <img src=/static/images/${item.image}></img>
-                    <p class="itemDesc">${item.description}</p>
-                    <h2 class="itemPrice">$ ${roundedCost}</h2>
-                    <button style="width: 5em; height: 5em"class="addListBtn">Add To Ticket!!</button>
-                </article>
-              `
-    );
-  }
-  buttons();
-}
-
 //the initialize function of the page
 function populateInitialContent() {
+  fetchingAndPopulating("starter", "Starters");
+  fetchingAndPopulating("small", "Burgers and More");
+  fetchingAndPopulating("big", "Plates");
+  fetchingAndPopulating("side", "Sides");
+  fetchingAndPopulating("salad", "Salads");
+  if (dt.getDay() == 5 || dt.getDay() == 6) {
+    fetchingAndPopulating("seafood", "Seafood");
+  }
+}
+
+function fetchingAndPopulating(foodType, foodName) {
   fetch("/static/food.json")
     .then((req) => req.json())
     .then((data) => {
-      var startersHeader = document.createElement("header");
-      startersHeader.innerHTML = "Starters";
-      startersHeader.classList.add("full-width");
-      foodbox.appendChild(startersHeader);
-      var leList = [];
-      for (const item of data) {
-        if (item.type == "starter") {
-          leList.push(item);
-        }
-      }
-      populateContent(leList);
-      var burgersHeader = document.createElement("header");
-      burgersHeader.innerHTML = "Burgers and More";
-      burgersHeader.classList.add("full-width");
-      foodbox.appendChild(burgersHeader);
-      var leList = [];
-      for (const item of data) {
-        if (item.type == "small") {
-          leList.push(item);
-        }
-      }
-      populateContent(leList);
       var platesHeader = document.createElement("header");
-      platesHeader.innerHTML = "Plates";
+      platesHeader.innerHTML = foodName;
       platesHeader.classList.add("full-width");
       foodbox.appendChild(platesHeader);
       var leList = [];
       for (const item of data) {
-        if (item.type == "big") {
+        if (item.type == foodType) {
           leList.push(item);
         }
       }
-      populateContent(leList);
-      var sidesHeader = document.createElement("header");
-      sidesHeader.innerHTML = "Sides";
-      sidesHeader.classList.add("full-width");
-      foodbox.appendChild(sidesHeader);
-      var leList = [];
-      for (const item of data) {
-        if (item.type == "side") {
-          sidesNames.push(item.name);
-          leList.push(item);
-        }
+      var itemsbox = document.createElement("div");
+      itemsbox.classList.add("items");
+      foodbox.appendChild(itemsbox);
+      for (const item of leList) {
+        var roundedCost = item.cost.toFixed(2);
+        itemsbox.insertAdjacentHTML(
+          "beforeend",
+          `
+                    <article id="itemBox" class="${item.type}">
+                        <h2 class="itemName">${item.name}</h2>
+                        <img src=/static/images/${item.image}></img>
+                        <p class="itemDesc">${item.description}</p>
+                        <h2 class="itemPrice">$ ${roundedCost}</h2>
+                        <button style="width: 5em; height: 5em"class="addListBtn">Add To Ticket!!</button>
+                    </article>
+                  `
+        );
       }
-      populateContent(leList);
-      var saladsHeader = document.createElement("header");
-      saladsHeader.innerHTML = "Salads";
-      saladsHeader.classList.add("full-width");
-      foodbox.appendChild(saladsHeader);
-      var leList = [];
-      for (const item of data) {
-        if (item.type == "salad") {
-          leList.push(item);
-        }
-      }
-      populateContent(leList);
-      if (dt.getDay() == 5 || dt.getDay() == 6) {
-        var seafoodHeader = document.createElement("header");
-        seafoodHeader.innerHTML = "Seafood";
-        seafoodHeader.classList.add("full-width");
-        foodbox.appendChild(seafoodHeader);
-        var leList = [];
-        for (const item of data) {
-          if (item.type == "seafood") {
-            leList.push(item);
-          }
-        }
-        populateContent(leList);
+      var AddItemBtns = document.getElementsByClassName("addListBtn");
+      for (let i = 0; i < AddItemBtns.length; i++) {
+        var button = AddItemBtns[i];
+        button.addEventListener("click", AddItem);
       }
     });
-  buttons();
-}
-
-//makes the add buttons work
-function buttons() {
-  var AddItemBtns = document.getElementsByClassName("addListBtn");
-  for (let i = 0; i < AddItemBtns.length; i++) {
-    var button = AddItemBtns[i];
-    button.addEventListener("click", AddItem);
-  }
 }
 
 //adding an item to the ticket
@@ -324,15 +216,15 @@ function openCheckout() {
   }
 }
 
-function ticketToggle() {
-  if (dt.getHours() * 100 + dt.getMinutes() >= 1630 && dt.getHours() < 19) {
-    document.getElementById("ticketSide").classList.toggle("show");
-    document.querySelector(".mainpage").classList.toggle("grid");
-    document.getElementById("total").classList.toggle("show");
-  } else {
-    document.querySelector(".afterHoursError").classList.toggle("show");
-  }
-}
+// function ticketToggle() {
+//   if (dt.getHours() * 100 + dt.getMinutes() >= 1630 && dt.getHours() < 19) {
+//     document.getElementById("ticketSide").classList.toggle("show");
+//     document.querySelector(".mainpage").classList.toggle("grid");
+//     document.getElementById("total").classList.toggle("show");
+//   } else {
+//     document.querySelector(".afterHoursError").classList.toggle("show");
+//   }
+// }
 
 //toggle menu
 function myFunction() {
