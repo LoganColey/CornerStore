04a5e9fd3ae79a37c9110734f4177a3cf72e4104
ryleechisholm <rchisholm22@basecampcodingacademy.org>
2022-04-28 08:33:59 -0500

    added cart to cartItem creation

--- app/views.py
+++ app/views.py
@@ -121,7 +121,7 @@ def populateMenu(request):
 
 def addToCart(request, itemname):
     itemFromMenu = menuItem.objects.get(name=itemname)
-    new_cart_item = cartItem(cost=itemFromMenu.cost,name=itemFromMenu.name, type=itemFromMenu.type)
+    new_cart_item = cartItem((Cart.objects.get(user=request.user)),cost=itemFromMenu.cost,name=itemFromMenu.name, type=itemFromMenu.type)
     new_cart_item.save()
     return render(request, 'food.html', {"menu": checkDate(), "cartNum": cartItem.objects.all().count()})
 
